4ac093579b14e2258567978046029978433faee2
akhiljayan29aj <58233202+akhiljayan29aj@users.noreply.github.com>
2020-05-28 17:39:27 +0530

    Update Ass01_AkhilJayan.py

--- Ass01_AkhilJayan.py
+++ Ass01_AkhilJayan.py
@@ -1,5 +1,5 @@
 ## Assignment 1
-#Q1
+#Q1. Find area and perimeter of right angled triangle.(Given: Base and Height taken from the user)
 base = float(input("Base: "))
 height = float(input("Height: "))
 area = 0.5*base*height
